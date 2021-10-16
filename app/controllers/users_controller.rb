7106bb33372db15e2a0aa6fd543aff9c74c75e2a
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-10-16 15:22:47 -0700

    auth password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params[:email] = user_params[:email].downcase.strip
+    # user_params[:email] = user_params[:email].downcase.strip
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit( :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
   
 
