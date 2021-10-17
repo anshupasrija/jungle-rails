723eac9a208f0942bee36033161942cc033519f7
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-10-17 12:51:58 -0700

    checked login and signup part

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)  
-    raise `#{user.inspect}`
+  
       if user.save     
         session[:user_id] = user.id
         redirect_to '/'
